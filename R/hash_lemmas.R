42ca39f5aa801070de59db95cabf7e02b967c8f8
Tyler <tyler.rinker@gmail.com>
2017-02-12 19:13:01 -0500

    remove special character from reference

--- R/hash_lemmas.R
+++ R/hash_lemmas.R
@@ -1,8 +1,8 @@
 #' Lemmatization List
 #'
-#' A dataset based on M\u{e}chura's (2016) English lemmatization list.  This
+#' A dataset based on Mechura's (2016) English lemmatization list.  This
 #' data set can be useful for join style lemma replacement of inflected token
-#' forms to their root lemmas.  While this is not a true morphalogical analysis
+#' forms to their root lemmas.  While this is not a true morphological analysis
 #' this style of lemma replacement is fast and typically still robust.
 #'
 #' @details
@@ -16,5 +16,5 @@
 #' @name hash_lemmas
 #' @usage data(hash_lemmas)
 #' @format A data frame with 41,533 rows and 2 variables
-#' @references M\u{e}chura, M. B. (2016). \emph{Lemmatization list: English (en)} [Data file]. Retrieved from \url{http://www.lexiconista.com}
+#' @references Mechura, M. B. (2016). \emph{Lemmatization list: English (en)} [Data file]. Retrieved from \url{http://www.lexiconista.com}
 NULL
