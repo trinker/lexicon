cde48b18e7f11671f7ac0707a2f3ef247e904da3
Tyler Rinker <trinker@campuslabs.com>
2018-12-08 17:07:23 -0500

    * `hash_emoticons` had '3' as 'laighing' when it should have been '=3'.  This   has been corrected. close #30

--- R/hash_nrc_emotion.R
+++ R/hash_nrc_emotion.R
@@ -15,5 +15,9 @@
 #' @name hash_nrc_emotions 
 #' @usage data(hash_nrc_emotions) 
 #' @format A data frame with 8265 rows and 2 variables 
-#' @references 
+#' @references http://www.purl.com/net/lexicons  \cr \cr
+#' Mohammad, S. M. & Turney, P. D. (2010) Emotions evoked by common words and
+#' phrases: Using Mechanical Turk to create an emotion lexicon, In Proceeding of
+#' Workshop on Computational Approaches to Analysis and Generation of Emotion in
+#' Text, 26-34.
 NULL 
